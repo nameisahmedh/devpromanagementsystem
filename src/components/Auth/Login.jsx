6d33b77cf9ee47d84b5db8c66ed7ab5297f164ef
Md Ahmed <mdqamarahmed123@gmail.com>
2025-07-21 11:30:45 +0530

    Updated Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -219,6 +219,7 @@ const Login = ({handleLogin}) => {
                 Contact your administrator
               </a>
             </div>
+          </motion.div>
           <p className="mt-8 text-xs text-center text-[#b8c1ec]">
             DevPro Solutions Staff Management System © 2025
             <br />
